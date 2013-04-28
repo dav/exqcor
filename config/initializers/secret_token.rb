d9278af6c533dec9d991e17c053b55fe81a85ed1
Dav Yaginuma <dav@akuaku.org>
2013-04-28 09:24:24 -0700

    DIY: rails init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Exqcor::Application.config.secret_token = 'cd489b8c4804f89440a2bd2576936e26ea5637501f6682407094631ed33616f97e1762f425f95add4d1557300d03880deb13853a5383032b51959477d777e03a'

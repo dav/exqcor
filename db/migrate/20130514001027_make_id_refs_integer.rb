bc322d77c4c329fb4f9c446951fbfdb35ba632f2
Dav Yaginuma <dav@akuaku.org>
2013-05-13 17:14:01 -0700

    DIY: pg wants int foreign key columns

---
+++ db/migrate/20130514001027_make_id_refs_integer.rb
@@ -0,0 +1,21 @@
+class MakeIdRefsInteger < ActiveRecord::Migration
+  def up
+    def change
+      change_column :characters, :play_id, :integer
+      change_column :props, :section_id, :integer
+      change_column :sections, :play_id, :integer
+      change_column :sub_sections, :section_id, :integer
+      change_column :lines, :sub_section_id, :integer
+      change_column :lines, :charactersection_id, :integer
+    end
+  end
+
+  def down
+    change_column :characters, :play_id, :string
+    change_column :props, :section_id, :string
+    change_column :sections, :play_id, :string
+    change_column :sub_sections, :section_id, :string
+    change_column :lines, :sub_section_id, :string
+    change_column :lines, :charactersection_id, :string
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130507035043) do
+ActiveRecord::Schema.define(:version => 20130514001027) do
 
   create_table "characters", :force => true do |t|
     t.string   "name"

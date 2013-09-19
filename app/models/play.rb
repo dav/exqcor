807a8cc38676f05190dfb8605595556b19b6a0a6
Dav Yaginuma <dav@akuaku.org>
2013-09-18 19:58:22 -0700

    Redesign List Plays screen.

--- app/models/play.rb
+++ app/models/play.rb
@@ -2,4 +2,15 @@ class Play < ActiveRecord::Base
   attr_accessible :description, :title
   has_many :characters, :inverse_of => :play, :dependent => :delete_all
   has_many :sections, :inverse_of => :play, :dependent => :delete_all
+  
+  def lines
+    lines = []
+    sections.each do |s|
+      s.sub_sections.each do |ss|
+        lines += ss.lines
+      end
+    end
+    lines
+  end
+  
 end

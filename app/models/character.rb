a3c06ebfb53334f651b2b9b004726c594304c316
Dav Yaginuma <dav@akuaku.org>
2013-09-25 11:08:11 -0700

    Don't show edit or delete for VOSD character [refs #50195739]

--- app/models/character.rb
+++ app/models/character.rb
@@ -1,7 +1,14 @@
 class Character < ActiveRecord::Base
+  before_destroy :confirm_not_vosd
+
   belongs_to :play, :inverse_of => :characters
   attr_accessible :description, :name
   has_many :lines, :inverse_of => :sub_section
   has_and_belongs_to_many :sections
   validates_uniqueness_of :name, scope: :play_id
+  
+  def confirm_not_vosd
+    return false if self.name == 'VOSD'
+    true
+  end
 end

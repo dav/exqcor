b56beb68c048e03b8a9a52da2ecf3a71bbdccf0a
dav <dav@akuaku.org>
2013-12-15 19:06:36 -0800

    ‘Go To Next Writer’ visibility improved [fixes #62216768]

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -22,3 +22,13 @@
 //= require_tree .//routers
 //= require_tree .
 //= require jquery.countdown.js
+
+
+jQuery.fn.center = function () {
+    this.css("position","absolute");
+    this.css("top", Math.max(0, (($(window).height() - $(this).outerHeight()) / 2) + 
+                                                $(window).scrollTop()) + "px");
+    this.css("left", Math.max(0, (($(window).width() - $(this).outerWidth()) / 2) + 
+                                                $(window).scrollLeft()) + "px");
+    return this;
+}
